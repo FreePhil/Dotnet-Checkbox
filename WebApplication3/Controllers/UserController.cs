1279c9c1fc631f26b3a49262634f0e530b621cc5
Phil <phil@hengetech.com.tw>
2024-06-20 10:28:20 +0800

    1st commit

---
+++ WebApplication3/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        var hobbies = new List<HobbyModel>
+        {
+            new HobbyModel { Name = "Jogging" },
+            new HobbyModel { Name = "Football" },
+            new HobbyModel { Name = "Basketball" },
+            new HobbyModel { Name = "Swimming" },
+            new HobbyModel { Name = "Baseball" }
+        };
+        ViewBag.Hobbies = hobbies;
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Submit(UserModel userModel)
+    {
+        var hobbies = new List<HobbyModel>
+        {
+            new HobbyModel { Name = "Jogging" },
+            new HobbyModel { Name = "Football" },
+            new HobbyModel { Name = "Basketball" },
+            new HobbyModel { Name = "Swimming" },
+            new HobbyModel { Name = "Baseball" }
+        };
+        ViewBag.Hobbies = hobbies;
+        return View("Index", userModel);
+    }
+
+    public ActionResult Success()
+    {
+        return View();
+    }
+}
---
+++ WebApplication3/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication3.Models;
+
+public class UserModel
+{
+    public string Username { get; set; }
+    public int Age { get; set; }
+    public List<HobbyModel> Hobbies { get; set; } = new List<HobbyModel>();
+    public List<String> HobbyNames { get; set; } = new List<String>();
+}
+
+public class HobbyModel
+{
+    public string Name { get; set; }
+    public bool IsSelected { get; set; }
+}
